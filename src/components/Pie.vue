3cc3be5beb9c63a3c2cc75f56b1ed01901c861c0
moshuying <1460083332@qq.com>
2019-06-15 12:21:08 +0800

    修改部分组件

---
+++ src/components/Pie.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="Line">
+        <p>Line的组件</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Line"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/main/Rake.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Rake"
+    }
+</script>
+
+<style scoped>
+
+</style>
