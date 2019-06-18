154fa1409de73b25d2cf8e74ae10a11b76ab192c
moshuying <1460083332@qq.com>
2019-06-18 19:29:21 +0800

    玫瑰图

--- src/components/charts/Bar.vue
+++ src/components/charts/Bar.vue
@@ -2,6 +2,9 @@
   <div class="Bar">
     <div id="chart1" class="chart"></div>
     <div id="chart2" class="chart"></div>
+    <h1>南丁格尔玫瑰图</h1>
+    <div id="chart3" class="chart"></div>
+    <div id="chart4" class="chart"></div>
   </div>
 </template>
 
@@ -16,6 +19,9 @@
     mounted() {
       this.drawLine();
       this.drawLine2();
+      this.drawLine3();
+      this.drawLine4();
+      this.drawLine5();
     },
     methods:{
       drawLine(){
@@ -164,6 +170,113 @@
             // Set `large` for large data amount
             large: true
           }]
+        });
+      },
+      drawLine3(){
+        let chart3=this.$echarts.init(document.getElementById('chart3'));
+        chart3.setOption({
+          title: {
+            text: '南丁格尔玫瑰图'
+            , subtext: '纯属虚构'
+            , x: 'center'
+          }
+          ,tooltip:{
+            x:'center'
+            ,y:'bottom'
+            ,data:['rose','rose1','rose3']
+          },toolbox: {
+            show : true,
+            feature : {
+              mark : {show: true},
+              dataView : {show: true, readOnly: false},
+              magicType : {
+                show: true,
+                type: ['pie', 'funnel']
+              },
+              restore : {show: true},
+              saveAsImage : {show: true}
+            }
+          },
+          calculable : true,
+          series : [
+            {
+              name:'半径模式',
+              type:'pie',
+              radius : [20, 110],
+              center : ['25%', '50%'],
+              roseType : 'radius',
+              label: {
+                normal: {
+                  show: false
+                },
+                emphasis: {
+                  show: true
+                }
+              },
+              lableLine: {
+                normal: {
+                  show: false
+                },
+                emphasis: {
+                  show: true
+                }
+              },
+              data:[
+                {value:10, name:'rose1'},
+                {value:5, name:'rose2'},
+                {value:15, name:'rose3'},
+                {value:25, name:'rose4'},
+                {value:20, name:'rose5'},
+                {value:35, name:'rose6'},
+                {value:30, name:'rose7'},
+                {value:40, name:'rose8'}
+              ]
+            },
+            {
+              name:'面积模式',
+              type:'pie',
+              radius : [30, 110],
+              center : ['75%', '50%'],
+              roseType : 'area',
+              data:[
+                {value:10, name:'rose1'},
+                {value:5, name:'rose2'},
+                {value:15, name:'rose3'},
+                {value:25, name:'rose4'},
+                {value:20, name:'rose5'},
+                {value:35, name:'rose6'},
+                {value:30, name:'rose7'},
+                {value:40, name:'rose8'}
+              ]
+            }
+          ]
+        });
+      },
+      drawLine4(){
+        let chart4 = this.$echarts.init(document.getElementById('chart4'),'macarons');
+        chart4.setOption({
+            legend: {},
+            tooltip: {},
+            dataset: {
+              // 提供一份数据。
+              source: [
+                ['product', '2015', '2016', '2017'],
+                ['Matcha Latte', 43.3, 85.8, 93.7],
+                ['Milk Tea', 83.1, 73.4, 55.1],
+                ['Cheese Cocoa', 86.4, 65.2, 82.5],
+                ['Walnut Brownie', 72.4, 53.9, 39.1]
+              ]
+            },
+            // 声明一个 X 轴，类目轴（category）。默认情况下，类目轴对应到 dataset 第一列。
+            xAxis: {type: 'category'},
+            // 声明一个 Y 轴，数值轴。
+            yAxis: {},
+            // 声明多个 bar 系列，默认情况下，每个系列会自动对应到 dataset 的每一列。
+            series: [
+              {type: 'bar'},
+              {type: 'bar'},
+              {type: 'bar'}
+            ]
         });
       }
     }
