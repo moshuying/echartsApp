81b461de06632e2deb17d53dfef4520e6beb04bb
moshuying <1460083332@qq.com>
2019-06-20 23:27:46 +0800

    练习

--- src/components/charts/Pie.vue
+++ src/components/charts/Pie.vue
@@ -1,162 +1,38 @@
 <template>
     <div class="Bar">
-        <div id="chart1" class="chart"></div>
+        <div id="chart2" class="chart"></div>
     </div>
 </template>
 
 <script>
     import 'echarts/theme/macarons'
-    import echarts from 'echarts'
     export default {
         name: "Pie",
         data() {
             return {}
         },
         mounted() {
-            this.drawLine();
+            this.drawLine1();
         },
         methods:{
-            drawLine(){
-                app.title = '柱状图框选';
-                let xAxisData = [];
-                let data1 = [];
-                let data2 = [];
-                let data3 = [];
-                let data4 = [];
-                for (let i = 0; i < 10; i++) {
-                    xAxisData.push('Class' + i);
-                    data1.push((Math.random() * 2).toFixed(2));
-                    data2.push(-Math.random().toFixed(2));
-                    data3.push((Math.random() * 5).toFixed(2));
-                    data4.push((Math.random() + 0.3).toFixed(2));
-                }
-                let itemStyle = {
-                    normal: {
-                    },
-                    emphasis: {
-                        barBorderWidth: 1,
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowOffsetY: 0,
-                        shadowColor: 'rgba(0,0,0,0.5)'
-                    }
-                };
-                let chart1=this.$echarts.init(document.getElementById('chart1'),this.$Global.mac);
-                chart1.setOption({
-                        backgroundColor: '#eee',
-                        legend: {
-                            data: ['bar', 'bar2', 'bar3', 'bar4'],
-                            align: 'left',
-                            left: 10
-                        },
-                        brush: {
-                            toolbox: ['rect', 'polygon', 'lineX', 'lineY', 'keep', 'clear'],
-                            xAxisIndex: 0
-                        },
-                        toolbox: {
-                            feature: {
-                                magicType: {
-                                    type: ['stack', 'tiled']
-                                },
-                                dataView: {}
-                            }
-                        },
-                        tooltip: {},
-                        xAxis: {
-                            data: xAxisData,
-                            name: 'X Axis',
-                            silent: false,
-                            axisLine: {onZero: true},
-                            splitLine: {show: false},
-                            splitArea: {show: false}
-                        },
-                        yAxis: {
-                            inverse: true,
-                            splitArea: {show: false}
-                        },
-                        grid: {
-                            left: 100
-                        },
-                        visualMap: {
-                            type: 'continuous',
-                            dimension: 1,
-                            text: ['High', 'Low'],
-                            inverse: true,
-                            itemHeight: 200,
-                            calculable: true,
-                            min: -2,
-                            max: 6,
-                            top: 60,
-                            left: 10,
-                            inRange: {
-                                colorLightness: [0.4, 0.8]
-                            },
-                            outOfRange: {
-                                color: '#bbb'
-                            },
-                            controller: {
-                                inRange: {
-                                    color: '#2f4554'
-                                }
-                            }
-                        },
-                        series: [
-                            {
-                                name: 'bar',
-                                type: 'bar',
-                                stack: 'one',
-                                itemStyle: itemStyle,
-                                data: data1
-                            },
-                            {
-                                name: 'bar2',
-                                type: 'bar',
-                                stack: 'one',
-                                itemStyle: itemStyle,
-                                data: data2
-                            },
-                            {
-                                name: 'bar3',
-                                type: 'bar',
-                                stack: 'two',
-                                itemStyle: itemStyle,
-                                data: data3
-                            },
-                            {
-                                name: 'bar4',
-                                type: 'bar',
-                                stack: 'two',
-                                itemStyle: itemStyle,
-                                data: data4
-                            }
-                        ]
-                    });
-
-                chart1.on('brushSelected', renderBrushed);
-
-                function renderBrushed(params) {
-                    let brushed = [];
-                    let brushComponent = params.batch[0];
-
-                    for (let sIdx = 0; sIdx < brushComponent.selected.length; sIdx++) {
-                        let rawIndices = brushComponent.selected[sIdx].dataIndex;
-                        brushed.push('[Series ' + sIdx + '] ' + rawIndices.join(', '));
-                    }
-
-                    chart1.setOption({
-                        title: {
-                            backgroundColor: '#333',
-                            text: 'SELECTED DATA INDICES: \n' + brushed.join('\n'),
-                            bottom: 0,
-                            right: 0,
-                            width: 100,
-                            textStyle: {
-                                fontSize: 12,
-                                color: '#fff'
-                            }
+            drawLine1(){
+                let chart2=this.$echarts.init(document.getElementById('chart2'));
+                chart2.setOption({
+                    series : [
+                        {
+                            name: '访问来源',
+                            type: 'pie',
+                            radius: '55%',
+                            data:[
+                                {value:235, name:'视频广告'},
+                                {value:274, name:'联盟广告'},
+                                {value:310, name:'邮件营销'},
+                                {value:335, name:'直接访问'},
+                                {value:400, name:'搜索引擎'}
+                            ]
                         }
-                    });
-                }
+                    ]
+                });
             }
         }
     }
