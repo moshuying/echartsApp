861b276d4ca5baf9ddd2a3d45c7a67bcce035d5d
moshuying <1460083332@qq.com>
2019-07-10 23:02:39 +0800

    模块开发

---
+++ src/components/appStore/appModel.vue
@@ -0,0 +1,25 @@
+<template>
+<p>test</p>
+</template>
+
+<script>
+    export default {
+        name: "appModel",
+        props: [''],
+        data() {
+            return {}
+        },
+        compoents: {},
+        computed: {},
+        beforeMount() {
+        },
+        mounted() {
+        },
+        methods: {},
+        watch: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/main/News.vue
+++ src/components/main/News.vue
@@ -1,10 +1,19 @@
 <template>
     <h1>News的组件</h1>
+    <appModel></appModel>
 </template>
 
-<script>
-    export default {
-        name: "News"
+<script lang="ts" >
+    import { Component, Vue } from 'vue-property-decorator';
+    import appModel from '../appStore/appModel';
+    
+    @Component({
+        components: {
+            'appModel':appModel,
+        },
+    })
+    export default class Home extends Vue{
+
     }
 </script>
 
