e46f0309ca2a8a25cd0ebcff9eee5be08b82181e
moshuying <1460083332@qq.com>
2019-07-12 07:47:02 +0800

    完成app下载模块组件

--- src/components/appStore/appModel.vue
+++ src/components/appStore/appModel.vue
@@ -2,10 +2,22 @@
 <div class="appModel">
     <div class="logo"></div>
     <div class="name">
-        <div class="appName">
-            <p>元气骑士</p>
+        <div class="middle">
+            <div class="appName">
+                <span>元气骑士</span>
+            </div>
+            <div class="starAndSize">
+                <div class="star fl">
+                    <span class="star-percent" style="width: 80%"></span>
+                </div>
+                <div class="size">
+                    <p>73MB</p>
+                </div>
+            </div>
+            <div class="synopsis">
+                <p>世界危在旦夕，时间处于剑与魔法afsdasdfasdf的时代，</p>
+            </div>
         </div>
-        <div class="star"></div>
     </div>
     <div class="install">
         <p>安装</p>
@@ -33,20 +45,65 @@
 @import "../../static/scss/mixin.scss";
 .appModel{
     padding: 1rem 0 0;
-    width: 95%;
+    width: 92%;
     margin: auto;
     @include Flex(space-between);
     .logo{
-        width: 5rem;
-        height: 5rem;
+        min-width: 5rem;
+        min-height: 5rem;
         background-image: url("../../static/images/元气骑士.png");
         background-size: cover;
     }
-    .name{}
+    .name{
+        width: 100%;
+        height: 100%;
+        align-self: center;
+        padding-left: 1rem;
+        text-align: left;
+        .middle{
+            .appName{
+                font-weight: 600;
+            }
+            .starAndSize{
+                position: relative;
+                left: -0.7rem;
+                padding-top: 0.3rem;
+                .star{
+                    width: 116px;
+                    height: 20px;
+                    transform: scale(0.8);
+                    vertical-align: top;
+                    background-image: url("../../static/images/star-24.png");
+                    background-position: 0 -144px;
+                    .star-percent{
+                        background-position: 0 -119px;
+                        height: 20px;
+                        display: inline-block;
+                        background-image: url("../../static/images/star-24.png");
+                        vertical-align: top;
+                    }
+                }
+                .size{
+                    height: 20px;
+                    @include Flex(flex-end,flex-start,nowrap,column);
+                }
+            }
+            .synopsis{
+                padding-top: 0.3rem;
+                max-width: 200px;
+                p{
+                    overflow: hidden;
+                    text-overflow:ellipsis;
+                    white-space: nowrap;
+                }
+            }
+        }
+    }
     .install{
+        min-width: 3.6rem;
         color: #3bd58b;
         p{
-            padding: 0.3rem 1rem 0.3rem ;
+            padding: 0.1rem 0.7rem 0.1rem ;
             background-color: #dff8ec;
             border-radius: 6px;
         }
