bc58d952d10799ffaa4a1f61f892191a51a1b8f8
moshuying <1460083332@qq.com>
2019-07-11 14:02:19 +0800

    复用组件构建完成

--- src/components/appStore/appModel.vue
+++ src/components/appStore/appModel.vue
@@ -1,7 +1,17 @@
 <template>
-<p>test</p>
+<div class="appModel">
+    <div class="logo"></div>
+    <div class="name">
+        <div class="appName">
+            <p>元气骑士</p>
+        </div>
+        <div class="star"></div>
+    </div>
+    <div class="install">
+        <p>安装</p>
+    </div>
+</div>
 </template>
-
 <script>
     export default {
         name: "appModel",
@@ -19,7 +29,28 @@
         watch: {}
     }
 </script>
-
-<style scoped>
+<style scoped lang="scss">
+@import "../../static/scss/mixin.scss";
+.appModel{
+    padding: 1rem 0 0;
+    width: 95%;
+    margin: auto;
+    @include Flex(space-between);
+    .logo{
+        width: 5rem;
+        height: 5rem;
+        background-image: url("../../static/images/元气骑士.png");
+        background-size: cover;
+    }
+    .name{}
+    .install{
+        color: #3bd58b;
+        p{
+            padding: 0.3rem 1rem 0.3rem ;
+            background-color: #dff8ec;
+            border-radius: 6px;
+        }
+    }
+}
 
 </style>
--- src/components/main/News.vue
+++ src/components/main/News.vue
@@ -1,15 +1,24 @@
 <template>
-    <h1>News的组件</h1>
-    <appModel></appModel>
+    <div class="news">
+        <v-appModel></v-appModel>
+        <v-appModel></v-appModel>
+        <v-appModel></v-appModel>
+        <v-appModel></v-appModel>
+        <v-appModel></v-appModel>
+        <v-appModel></v-appModel>
+        <v-appModel></v-appModel>
+
+        <h1>News的组件</h1>
+    </div>
 </template>
 
 <script lang="ts" >
     import { Component, Vue } from 'vue-property-decorator';
-    import appModel from '../appStore/appModel';
-    
+    import appModel from '../../components/appStore/appModel.vue';
+
     @Component({
         components: {
-            'appModel':appModel,
+            'v-appModel':appModel,
         },
     })
     export default class Home extends Vue{
